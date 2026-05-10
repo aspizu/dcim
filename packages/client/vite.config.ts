53f40578015a12d89e98d958da359f9b8dd08a0e
Priyanshu Dangare <hello@aspiz.uk>
2026-05-10 22:48:19 +0530

    fmt

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,7 +5,6 @@ import {tanstackRouter} from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import {defineConfig, loadEnv} from "vite"
 
-
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd())
