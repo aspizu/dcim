d4112a758d17c78f95a5a1e0cc9cb1a58831b7bc
Priyanshu Dangare <hello@aspiz.uk>
2026-05-13 18:18:41 +0530

    add basic album listing

--- packages/client/src/components/sidebar.tsx
+++ packages/client/src/components/sidebar.tsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
     <div className="flex flex-col gap-2">
       <h1 className="mt-4 text-sm font-medium">Albums</h1>
       {albums.data?.map((album) => (
-        <Link to="/a/$album" params={{album: album.id}}>
+        <Link key={album.id} to="/a/$album" params={{album: album.id}}>
           <div className="flex flex-col rounded-md bg-card px-2 py-1">
             <span className="w-full truncate">{album.name}</span>
             <span className="text-xs text-muted-foreground">
